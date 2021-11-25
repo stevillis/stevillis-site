c54c19df5658ea06e1859a9850f5b227fb2d5844
Stévillis Sousa <stevillis@hotmail.com>
2021-11-24 20:41:04 -0400

    Remove default colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      colors: {
-        shark: "#242426",
-        rose: "#ff0066",
-      },
+    extend: {},
+    colors: {
+      white: "#fff",
+      shark: "#242426",
+      rose: "#ff0066",
     },
   },
   variants: {
