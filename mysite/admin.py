2d32bfe5a3cfc9b894279974cbeaf581718dc861
Stévillis Sousa <stevillis@hotmail.com>
2021-12-19 18:09:05 -0400

    Create model Formation

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from mysite.models import Category, Institution
+from mysite.models import Category, Institution, Formation
 
 
 @admin.register(Category)
@@ -14,4 +14,11 @@ class CategoryAdmin(admin.ModelAdmin):
 class InstitutionAdmin(admin.ModelAdmin):
     fields = ['name']
     list_display = ('name', 'created_at', 'updated_at',)
+    search_fields = ['name']
+
+
+@admin.register(Formation)
+class FormationAdmin(admin.ModelAdmin):
+    fields = ['name', 'workload', 'curriculum_map', 'start_date', 'end_date']
+    list_display = ('name', 'workload', 'start_date', 'end_date', 'created_at', 'updated_at',)
     search_fields = ['name']
--- mysite/models.py
+++ mysite/models.py
@@ -4,16 +4,16 @@ from django.utils.translation import gettext_lazy as _
 
 
 class BaseModel(models.Model):
-    created_at = models.DateTimeField(_("Created at"), auto_now_add=True)
-    updated_at = models.DateTimeField(_("Updated at"), auto_now=True)
+    created_at = models.DateTimeField(_('Created at'), auto_now_add=True)
+    updated_at = models.DateTimeField(_('Updated at'), auto_now=True)
 
     class Meta:
         abstract = True
 
 
 class Category(BaseModel):
-    id_category = models.AutoField(_("Category ID"), primary_key=True)
-    name = models.CharField(_("Name"), max_length=40)
+    id_category = models.AutoField(_('Category ID'), primary_key=True)
+    name = models.CharField(_('Name'), max_length=40)
 
     class Meta:
         db_table = 'category'
@@ -25,13 +25,30 @@ class Category(BaseModel):
 
 
 class Institution(BaseModel):
-    id_institution = models.AutoField(_("Institution ID"), primary_key=True)
-    name = models.CharField(_("Name"), max_length=40)
+    id_institution = models.AutoField(_('Institution ID'), primary_key=True)
+    name = models.CharField(_('Name'), max_length=40)
 
     class Meta:
         db_table = 'institution'
         verbose_name = _('Institution')
         verbose_name_plural = _('Instituitions')
+
+    def __str__(self):
+        return self.name
+
+
+class Formation(BaseModel):
+    id_formation = models.AutoField(_('Formation ID'), primary_key=True)
+    name = models.CharField(_('Name'), max_length=100)
+    workload = models.CharField(_('Workload'), max_length=40)
+    curriculum_map = models.TextField(_('Curriculum Map'))
+    start_date = models.DateTimeField(_('Start Date'))
+    end_date = models.DateTimeField(_('End Date'))
+
+    class Meta:
+        db_table = 'formation'
+        verbose_name = _('Formation')
+        verbose_name_plural = _('Formations')
 
     def __str__(self):
         return self.name
