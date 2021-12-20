56c799a68b5a90052911af4ac3c1167600d53d95
Stévillis Sousa <stevillis@hotmail.com>
2021-12-20 00:49:07 -0400

    Add filters to Course model on Django Admin

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
 from mysite.models import Category, Course, Formation, Institution
+
+
+def custom_titled_filter(title):
+    class Wrapper(admin.FieldListFilter):
+        def __new__(cls, *args, **kwargs):
+            instance = admin.FieldListFilter.create(*args, **kwargs)
+            instance.title = title
+            return instance
+
+    return Wrapper
 
 
 @admin.register(Category)
@@ -28,4 +39,9 @@ class FormationAdmin(admin.ModelAdmin):
 class CourseAdmin(admin.ModelAdmin):
     fields = ['name', 'workload', 'curriculum_map', 'start_date', 'end_date', 'institution', 'categories', 'formation']
     list_display = ('name', 'workload', 'start_date', 'end_date', 'created_at', 'updated_at', 'institution',)
+    list_filter = [
+        ('institution', custom_titled_filter(_('Institution'))),
+        ('formation', custom_titled_filter(_('Formation'))),
+        ('categories', custom_titled_filter(_('Category')))
+    ]
     search_fields = ['name']
