2eca768bed4cb8f9bbf737488c038e8d5b64e5f4
Stévillis Sousa <stevillis@hotmail.com>
2022-04-05 17:49:54 -0400

    Remove Projects from menu

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,7 +4,7 @@ from .views import course, courses, formations, index, projects
 
 urlpatterns = [
     path("", index, name="index"),
-    path("projects/", projects, name="projects"),
+    # path("projects/", projects, name="projects"),
     path("formations/", formations, name="formations"),
     path("course/<int:pk>", course, name="course"),
     path("courses/", courses, name="courses"),
