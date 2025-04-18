dcfeb028763cc619bfaf9a9509bc5a1549c1caa2
Stévillis Sousa <stevillis@hotmail.com>
2025-04-18 03:25:55 -0400

    fix: remove sqlite from database

--- stevillis_site/settings.py
+++ stevillis_site/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+
 import os
 from pathlib import Path
 
@@ -95,13 +96,7 @@ else:
         "PORT": os.getenv("PGPORT"),
     }
 
-DATABASES = {
-    "default": default_database,
-    "sqlite": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    },
-}
+DATABASES = {"default": default_database}
 
 
 # Password validation
