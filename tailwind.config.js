d02e6a6c97b496293fafa457e9f5713b7cb54cd3
Stévillis Sousa <stevillis@hotmail.com>
2025-11-27 03:15:31 -0400

    fix: extend tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,16 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-    colors: {
-      white: "#fff",
-      shark: "#242426",
-      rose: "#ff0066",
-      tuna: "#353537",
-      amber: "#ffc107",
-      emperor: "#505050",
-      springgreen: "#00ff99",
-      silver: "#ccc",
+    extend: {
+      colors: {
+        shark: '#242426',
+        rose: '#ff0066',
+        tuna: '#353537',
+        amber: '#ffc107',
+        emperor: '#505050',
+        springgreen: '#00ff99',
+        silver: '#ccc',
+      },
     },
   },
   variants: {
